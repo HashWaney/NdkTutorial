039f571e8b763c00af64ac931c1243545535f7c4
Hash <HashWaney@gmail.com>
2020-06-26 22:24:24 +0800

    native-media

---
+++ native-media/src/main/java/me/hash/ndk/native_media/MainActivity.java
@@ -0,0 +1,14 @@
+package me.hash.ndk.native_media;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
